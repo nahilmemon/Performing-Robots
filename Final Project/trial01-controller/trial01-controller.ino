9a7545e0c9eb41fdbef9472280e7f081e3e940bd
nahilmemon <nam432@nyu.edu>
2017-11-26 22:08:45 +0400

    Code for the controller and navigator with xbees.
    The controller sends info to the navigator with an xbee. The navigator receives info from the controller with an xbee. But, there's an issue where, whenever the navigator arduino is restarted, the message gets shifted.

---
+++ Final Project/trial01-controller/trial01-controller.ino
@@ -0,0 +1,343 @@
+// ------ LIBRARIES ------ //
+// We'll use SoftwareSerial to communicate with the XBee:
+#include <SoftwareSerial.h>
+// XBee's DOUT (TX) is connected to pin 2 (Arduino's Software RX)
+// XBee's DIN (RX) is connected to pin 3 (Arduino's Software TX)
+SoftwareSerial XBee(2, 3); // RX, TX
+// ------ GLOBAL VARIABLES ------ //
+// --- NAVIGATOR --- //
+// Navigator Button Stuff
+// for each button, the relevant info is stored in an array
+// 0: pin number, 1: current button state, 2: last button state, 3: tell other functions to do stuff
+int buttonIncrementMessagesIndex[4] = {4, 0, 0, 0};
+int buttonDecrementMessagesIndex[4] = {5, 0, 0, 0};
+int buttonReadyToDisplayMessage[4] = {6, 0, 0, 0};
+// Navigator Message Stuff
+const int lengthOfNavigatorMessagesArray = 6;
+String navigatorMessagesArray[lengthOfNavigatorMessagesArray] = {"Hello, I'm string 1.", "Bonjour, je suis string 2.", 
+"Hi, I'm string 3.", "Salut, je suis string 4.", "Goodbye, said string 5.", "Au revoir, said string 6"};
+int navigatorMessagesIndex = 0;
+
+// --- WANDERER --- //
+// --- Variables to Control the Wanderer's Movements and Fan --- //
+// Wanderer Inputs
+const int joystickXPin = 0;
+const int joystickYPin = 1;
+int joystickXValue = 0;
+int joystickYValue = 0;
+// for each button, the relevant info is stored in an array
+// 0: pin number, 1: current button state, 2: last button state, 3: tell other functions to do stuff
+int buttonToggleFan[4] = {7, 0, 0, 0};
+int buttonIncrementSpeedReductionFactor[4] = {8, 0, 0, 0};
+int buttonDecrementSpeedReductionFactor[4] = {9, 0, 0, 0};
+int buttonToggleUnisonMode[4] = {10, 0, 0, 0};
+
+// Fan
+int fanOn = 0;
+// Move in unison mode
+int moveInUnisonMode = 0;
+//0: unison mode off, 1-5: unison mode on
+//1: up, 2: down, 3: left, 4: right, 5: stop
+int navigatorDirectionCode = 0;  
+
+// --- Variables to Control the DC Motors' Speed --- //
+// need to slow down the left motor so that its speed matches up with the speed of the right motor
+// so I'll use the below factor to multiply this variable with the speed of the left motor to slow it down
+int speedReductionFactor = 48; //0.465; // 0.448 0.45 with battery
+
+// ------ SETUP ------ //
+void setup() {
+  XBee.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Controller commencing...");
+  // Set up button pins as inputs
+  pinMode(buttonIncrementMessagesIndex[0],INPUT);
+  pinMode(buttonDecrementMessagesIndex[0],INPUT);
+  pinMode(buttonReadyToDisplayMessage[0],INPUT);
+  pinMode(buttonToggleFan[0],INPUT);
+  pinMode(buttonIncrementSpeedReductionFactor[0],INPUT);
+  pinMode(buttonDecrementSpeedReductionFactor[0],INPUT);
+  pinMode(buttonToggleUnisonMode[0],INPUT);
+}
+
+// ------ LOOP ------ //
+void loop() {
+//  Serial.print(digitalRead(buttonIncrementMessagesIndex[0]));
+//  Serial.print(digitalRead(buttonDecrementMessagesIndex[0]));
+//  Serial.print(digitalRead(buttonReadyToDisplayMessage[0]));
+//  Serial.print(digitalRead(buttonToggleFan[0]));
+//  Serial.print(digitalRead(buttonIncrementSpeedReductionFactor[0]));
+//  Serial.print(digitalRead(buttonDecrementSpeedReductionFactor[0]));
+//  Serial.print(digitalRead(buttonToggleUnisonMode[0]));
+//  Serial.print(analogRead(joystickXPin));
+//  Serial.println(analogRead(joystickYPin));
+
+  // --- NAVIGATOR --- //
+  // 1. increment the array index 
+  // 2. decrement the array index
+  // 3. check if message is ready to be sent
+  // 4. send info through xbee
+
+  // 1. increment the array index 
+  buttonEdgeStateDetection(buttonIncrementMessagesIndex);
+  navigatorMessagesIndex = incrementNavigatorMessagesIndex(navigatorMessagesIndex, buttonIncrementMessagesIndex[3], lengthOfNavigatorMessagesArray-1);    
+  // 2. decrement the array index
+  buttonEdgeStateDetection(buttonDecrementMessagesIndex);
+  navigatorMessagesIndex = decrementNavigatorMessagesIndex(navigatorMessagesIndex, buttonDecrementMessagesIndex[3]);
+  // 3. check if message is ready to be sent
+  buttonEdgeStateDetection(buttonReadyToDisplayMessage);
+  // 4. send info through xbee
+  // Sending stuff to XBee N for the navigator
+  String navigatorMessagesIndexString = String(navigatorMessagesIndex);
+  String buttonReadyToDisplayMessageString = String(buttonReadyToDisplayMessage[3]);
+  XBee.print(navigatorMessagesIndexString);
+  XBee.print(',');
+  XBee.print(buttonReadyToDisplayMessageString);
+  XBee.print(',');
+
+  Serial.print(navigatorMessagesIndexString + ", " + buttonReadyToDisplayMessageString + ", ");
+
+  // --- WANDERER --- //
+  // 1. toggle fan on/off
+  // 2. toggle unison mode on/off
+  // 3. increment speed reduction factor
+  // 4. decrement speed reduction factor
+  // 5. multiply speed reduction by 100
+  // 6. read joystick x 
+  // 7. read joystick y
+  // 8. figure out direction code
+  // 9. send info through xbee
+
+  // 1. toggle fan on/off
+  buttonEdgeStateDetection(buttonToggleFan);
+  fanOn = toggleFanBasedOnButtonInput(buttonToggleFan[3], fanOn);
+  // 2. toggle unison mode on/off
+  buttonEdgeStateDetection(buttonToggleUnisonMode);
+  moveInUnisonMode = toggleUnisonModeBasedOnButtonInput(buttonToggleUnisonMode[3], moveInUnisonMode);
+  // 3. increment speed reduction factor
+  buttonEdgeStateDetection(buttonIncrementSpeedReductionFactor);
+  speedReductionFactor = incrementSpeedReductionFactor(buttonIncrementSpeedReductionFactor[3], speedReductionFactor);
+  // 4. decrement speed reduction factor
+  buttonEdgeStateDetection(buttonDecrementSpeedReductionFactor);
+  speedReductionFactor = decrementSpeedReductionFactor(buttonDecrementSpeedReductionFactor[3], speedReductionFactor); 
+  // 5. multiply speed reduction by 100
+//  speedReductionFactor *= 100;
+  // 6. read joystick x
+  joystickXValue = readAndSendJoystickXValue(joystickXPin);
+  // 7. read joystick y
+  joystickYValue = readAndSendJoystickYValue(joystickYPin);
+  // 8. figure out direction code
+  readJoystickXYValues(joystickXPin, joystickYPin);
+  // 9. send info through xbee
+  // Sending stuff to XBee W for the wanderer
+  String navigatorDirectionCodeString = String(navigatorDirectionCode);
+  String fanOnString = String(fanOn);
+  String joystickXValueString = String(joystickXValue);
+  String joystickYValueString = String(joystickYValue);
+  String speedReductionFactorString = String(speedReductionFactor);
+  XBee.print(navigatorDirectionCodeString);
+  XBee.print(',');
+  XBee.print(fanOnString);
+  XBee.print(',');
+  XBee.print(joystickXValueString);
+  XBee.print(',');
+  XBee.print(joystickYValueString);
+  XBee.print(',');
+  XBee.print(speedReductionFactorString);
+  XBee.print('\n');
+
+  Serial.println(navigatorDirectionCodeString + ", " + fanOnString + ", " + joystickXValueString + ", " + joystickYValueString + ", " + speedReductionFactorString);
+}
+
+// ------ CUSTOM FUNCTIONS ------ //
+// ------ NAVIGATOR ------ //
+void buttonEdgeStateDetection(int buttonInfoArray[]) {
+  // read the pushbutton input pin:
+  int buttonState = digitalRead(buttonInfoArray[0]);
+  int lastButtonState = buttonInfoArray[2];
+  buttonInfoArray[3] = 0;
+  
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+//      Serial.println("on");
+      buttonInfoArray[3] = 1;
+    } else {
+      // if the current state is LOW then the button went from on to off:
+//      Serial.println("off");
+      buttonInfoArray[3] = 0;
+    }
+  }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
+
+  buttonInfoArray[1] = buttonState;
+  buttonInfoArray[2] = lastButtonState;
+}
+
+// 6. Input: 1. index of the array, state of the button that will affect the index, length of the array - 1
+// Output: update the counter whenever the relevant button is pressed
+int incrementNavigatorMessagesIndex(int counter, int buttonStateChanged, int counterMaxLimit) {
+  // update the counter if the button was pressed
+  if (buttonStateChanged == 1) {
+    counter++;
+    Serial.println("----------------------------------");
+  }
+  // don't let the counter go beyond the limits of the array's length
+  if (counter >= counterMaxLimit) {
+    counter=counterMaxLimit;
+  }
+  //Serial.println(counter);
+  return counter;
+}
+
+// 7. Input: 1. index of the array, state of the button that will affect the index, length of the array - 1
+// Output: update the counter whenever the relevant button is pressed
+int decrementNavigatorMessagesIndex(int counter, int buttonStateChanged) {
+  // update the counter if the button was pressed
+  if (buttonStateChanged == 1) {
+    counter--;
+    Serial.println("********************************"); 
+  }
+  // don't let the counter go beyond the limits of the array's length
+  if (counter <= 0) {
+    counter=0;
+  }
+//  Serial.print("Counter: ");
+//  Serial.println(counter);
+  return counter;
+}
+
+// -------------------------------------------------------- //
+// --- WANDERER CONTROLS --- //
+// --- Functions to use the joystick's x and y axis inputs --- //
+int readAndSendJoystickXValue(int inputPinX) {
+  int inputValueX = analogRead(inputPinX);
+  return inputValueX/4;
+}
+
+int readAndSendJoystickYValue(int inputPinY) {
+  int inputValueY = analogRead(inputPinY);
+  return inputValueY/4;
+}
+
+void readJoystickXYValues(int inputPinX, int inputPinY) {
+  // x-value stuff
+  int inputValueX = analogRead(inputPinX);
+  int leftMinValue = 0;
+  int leftMaxValue = 490;
+  int rightMinValue = 530;
+  int rightMaxValue = 1023;
+  // y-value stuff
+  int inputValueY = analogRead(inputPinY);
+  int backwardMinValue = 0;
+  int backwardMaxValue = 490;
+  int forwardMinValue = 530;
+  int forwardMaxValue = 1023;
+
+  // if the x- and y-values are withing resting state, then stop the robot
+  if (inputValueX < rightMinValue && inputValueX > leftMaxValue && inputValueY < forwardMinValue && inputValueY > backwardMaxValue) {
+    if (moveInUnisonMode == 1) {
+      navigatorDirectionCode = 5;
+    } else {
+      navigatorDirectionCode = 0;
+    }
+//    Serial.print("stop.");
+  }
+  // else, let the robot move in the desired direction(s)
+  else {
+    // if the x-value is greater than the resting value, then turn right
+    if (inputValueX > rightMinValue) {
+      if (moveInUnisonMode == 1) {
+        navigatorDirectionCode = 4;
+      } else {
+        navigatorDirectionCode = 0;
+      }
+//      Serial.print("turn right.");
+    } 
+    // else if the x-value is less than the resting value, then turn left
+    if (inputValueX < leftMaxValue) {
+      if (moveInUnisonMode == 1) {
+        navigatorDirectionCode = 3;
+      } else {
+        navigatorDirectionCode = 0;
+      }
+//      Serial.print("turn left.");
+    } 
+    // if the y-value is greater than the resting value, then move forward
+    if (inputValueY > forwardMinValue) {
+      if (moveInUnisonMode == 1) {
+        navigatorDirectionCode = 1;
+      } else {
+        navigatorDirectionCode = 0;
+      }
+//      Serial.print("go forward.");
+    } 
+    // else if the y-value is less than the resting value, then move backward
+    if (inputValueY < backwardMaxValue) {
+      if (moveInUnisonMode == 1) {
+        navigatorDirectionCode = 2;
+      } else {
+        navigatorDirectionCode = 0;
+      }
+//      Serial.print("go backward.");
+    } 
+  }
+}
+
+// --- Functions for reading buttons, and their consequences --- //
+int toggleFanBasedOnButtonInput(int buttonStateChanged, int fanIsOn) {
+  // update the counter if the button was pressed
+  if (buttonStateChanged == 1) {
+    if (fanIsOn == 1) {
+      fanIsOn = 0;
+      // turn off the fan  
+      //fanMotor->run(RELEASE);
+    } else {
+      fanIsOn = 1;
+      // turn on the fan
+      //fanMotor->run(FORWARD);
+    }
+  }
+//  Serial.print(" Fan state: ");
+//  Serial.print(fanIsOn);
+  return fanIsOn;
+}
+
+int toggleUnisonModeBasedOnButtonInput(int buttonStateChanged, int unisonModeIsOn) {
+  // update the counter if the button was pressed
+  if (buttonStateChanged == 1) {
+    unisonModeIsOn = !unisonModeIsOn;
+  }
+//  Serial.print(" Unison mode: ");
+//  Serial.print(unisonModeIsOn);
+  return unisonModeIsOn;
+}
+
+int incrementSpeedReductionFactor(int buttonStateChanged, float reductionFactor) {
+  int upperLimit = 100;
+  // increase the reduction factor if the button was pressed
+  if (buttonStateChanged == 1) {
+    reductionFactor += 1;
+  }
+  // prevent the reduction factor from becoming too high
+  if (reductionFactor >= upperLimit) {
+    reductionFactor = upperLimit;
+  }
+  return reductionFactor;
+}
+
+int decrementSpeedReductionFactor(int buttonStateChanged, float reductionFactor) {
+  int lowerLimit = 0;
+  // decrease the reduction factor if the button was pressed
+  if (buttonStateChanged == 1) {
+    reductionFactor -= 1;
+  }
+  // prevent the reduction factor from becoming too low
+  if (reductionFactor <= lowerLimit) {
+    reductionFactor = lowerLimit;
+  }
+  return reductionFactor;
+}
---
+++ Final Project/trial01-navigator/trial01-navigator.ino
@@ -0,0 +1,291 @@
+// ------ LIBRARIES ------ //
+// We'll use SoftwareSerial to communicate with the XBee:
+#include <SoftwareSerial.h>
+// XBee's DOUT (TX) is connected to pin 2 (Arduino's Software RX)
+// XBee's DIN (RX) is connected to pin 3 (Arduino's Software TX)
+SoftwareSerial XBee(2, 3); // RX, TX
+// Servo Library
+#include <Servo.h>
+
+// ------ GLOBAL VARIABLES ------ //
+// Servo Objects
+Servo leftServo;  // create servo object to control left servo
+Servo rightServo;  // create servo object to control right servo
+int servoDelay = 500;
+const int servoMaxDelay = 500;
+// Navigator Message Stuff
+const int lengthOfNavigatorMessagesArray = 6;
+String navigatorMessagesArray[lengthOfNavigatorMessagesArray] = {"Hello, I'm string 1.", "Bonjour, je suis string 2.", 
+"Hi, I'm string 3.", "Salut, je suis string 4.", "Goodbye, said string 5.", "Au revoir, said string 6"};
+int navigatorMessagesIndex = 0;
+int messageReadyToBePlayed = 0;
+// Unison mode stuff
+int directionCodeFromWanderer = 0;
+
+// ------ SETUP ------ //
+void setup() {
+  XBee.begin(9600);
+  Serial.begin(9600);
+  leftServo.attach(9);  // attaches the servo on pin 9 to the servo object
+  rightServo.attach(10); // attaches the servo on pin 10 to the servo object
+  Serial.println("Navigator Starting...");
+}
+
+// ------ LOOP ------ //
+void loop() {
+  // A. read data coming from XBee C
+    // 0. Input 0: record the index of the array
+    // 1. Input 1: record whether the message is ready to be played
+    // 2. Input 2: record the direction code
+  // B. play the message based on the unison mode
+
+  // A. read data coming from XBee C
+  // read incoming data from Xbee C
+  if (XBee.available()) { // if data comes from the XBee
+    //Serial.write(XBee.read());
+    // --- Navigator Stuff --- //
+    // 0. Input 0: record the index of the array
+    navigatorMessagesIndex = XBee.parseInt();
+    // 1. Input 1: record whether the message is ready to be played
+    messageReadyToBePlayed = XBee.parseInt();
+    // 2. Input 2: record the direction code
+    directionCodeFromWanderer = XBee.parseInt();
+    // --- Wanderer Stuff --- //
+    // 3. Input 3: record the fan state
+    int fanOn = XBee.parseInt();
+    // 4. Input 4: record the joystick x value
+    int joystickXValue = XBee.parseInt();
+    // 5. Input 5: record the joystick y value
+    int joystickYValue = XBee.parseInt();
+    // 6. Input 6: record the speed reduction factor
+    int speedReductionFactor = XBee.parseInt();
+
+    Serial.println(String(navigatorMessagesIndex) + ", " + String(messageReadyToBePlayed) + ", " + String(directionCodeFromWanderer) + ", " + String(fanOn) + ", " + String(joystickXValue) + ", " + String(joystickYValue) + ", " + String(speedReductionFactor));
+    
+    // After all the inputs have been parsed, perform the desired operations with these inputs
+    if (XBee.read() == '\n') {
+      //B. play the message based on the unison mode
+      playMessageBasedOnMode(directionCodeFromWanderer);
+//      Serial.println("XBee Message Complete --------------------------------------------");
+      
+    }
+  }
+
+//  Serial.println("Outside XBee availability");
+}
+
+// ------ CUSTOM FUNCTIONS ------ //
+// ------ NAVIGATOR ------ //
+// 1. Input: left and right flag positions in degrees
+// Output: move the servo paddles with flags attached to the desired positions
+void moveFlags(int leftFlagPos, int rightFlagPos) {
+  leftServo.write(leftFlagPos);
+  rightServo.write(rightFlagPos);
+  Serial.print("Flag Positions: ");
+  Serial.print(leftFlagPos/30+1);
+  Serial.print(", ");
+  Serial.println(rightFlagPos/30+1);
+  delay(servoDelay);
+}
+
+// 2. Input: a character
+// Output: uses a switch case to determine which flag positions to use based on the character
+// Then it calls the moveFlags function
+void convertCharToFlagMovement(char inputCharacter) {
+  // Possible Servo Positions
+  int leftServoPos1 = 0;
+  int leftServoPos2 = 30;
+  int leftServoPos3 = 60;
+  int leftServoPos4 = 90;
+  int leftServoPos5 = 120;
+  int leftServoPos6 = 150;
+  int leftServoPos7 = 180;
+  int rightServoPos1 = 0;
+  int rightServoPos2 = 30;
+  int rightServoPos3 = 60;
+  int rightServoPos4 = 90;
+  int rightServoPos5 = 120;
+  int rightServoPos6 = 150;
+  int rightServoPos7 = 180;
+
+  // Convert input character to moveFlags(leftServoPos, rightServoPos)
+  switch(inputCharacter) {
+    case 'a': moveFlags(leftServoPos1, rightServoPos1);
+              break;
+    case 'b': moveFlags(leftServoPos1, rightServoPos2);
+              break;
+    case 'c': moveFlags(leftServoPos1, rightServoPos3);
+              break;
+    case 'd': moveFlags(leftServoPos1, rightServoPos4);
+              break;
+    case 'e': moveFlags(leftServoPos1, rightServoPos5);
+              break;
+    case 'f': moveFlags(leftServoPos1, rightServoPos6);
+              break;
+    case 'g': moveFlags(leftServoPos1, rightServoPos7);
+              break;
+    case 'h': moveFlags(leftServoPos2, rightServoPos1);
+              break;
+    case 'i': moveFlags(leftServoPos2, rightServoPos2);
+              break;
+    case 'j': moveFlags(leftServoPos2, rightServoPos3);
+              break;
+    case 'k': moveFlags(leftServoPos2, rightServoPos4);
+              break;
+    case 'l': moveFlags(leftServoPos2, rightServoPos5);
+              break;
+    case 'm': moveFlags(leftServoPos2, rightServoPos6);
+              break;
+    case 'n': moveFlags(leftServoPos2, rightServoPos7);
+              break;
+    case 'o': moveFlags(leftServoPos3, rightServoPos1);
+              break;
+    case 'p': moveFlags(leftServoPos3, rightServoPos2);
+              break;
+    case 'q': moveFlags(leftServoPos3, rightServoPos3);
+              break;
+    case 'r': moveFlags(leftServoPos3, rightServoPos4);
+              break;
+    case 's': moveFlags(leftServoPos3, rightServoPos5);
+              break;
+    case 't': moveFlags(leftServoPos3, rightServoPos6);
+              break;
+    case 'u': moveFlags(leftServoPos3, rightServoPos7);
+              break;
+    case 'v': moveFlags(leftServoPos4, rightServoPos1);
+              break;
+    case 'w': moveFlags(leftServoPos4, rightServoPos2);
+              break;
+    case 'x': moveFlags(leftServoPos4, rightServoPos3);
+              break;
+    case 'y': moveFlags(leftServoPos4, rightServoPos4);
+              break;
+    case 'z': moveFlags(leftServoPos4, rightServoPos5);
+              break;
+    case '0': moveFlags(leftServoPos4, rightServoPos6);
+              break;
+    case '1': moveFlags(leftServoPos4, rightServoPos7);
+              break;
+    case '2': moveFlags(leftServoPos5, rightServoPos1);
+              break;
+    case '3': moveFlags(leftServoPos5, rightServoPos2);
+              break;
+    case '4': moveFlags(leftServoPos5, rightServoPos3);
+              break;
+    case '5': moveFlags(leftServoPos5, rightServoPos4);
+              break;
+    case '6': moveFlags(leftServoPos5, rightServoPos5);
+              break;
+    case '7': moveFlags(leftServoPos5, rightServoPos6);
+              break;
+    case '8': moveFlags(leftServoPos5, rightServoPos7);
+              break;
+    case '9': moveFlags(leftServoPos6, rightServoPos1);
+              break;
+    case ' ': moveFlags(leftServoPos6, rightServoPos2);
+              break;
+    case '.': moveFlags(leftServoPos6, rightServoPos3);
+              break;
+    case ',': moveFlags(leftServoPos6, rightServoPos4);
+              break;
+    case '?': moveFlags(leftServoPos6, rightServoPos5);
+              break;
+    case '!': moveFlags(leftServoPos6, rightServoPos6);
+              break;
+    case 'U': moveFlags(leftServoPos6, rightServoPos7);
+              break;
+    case 'D': moveFlags(leftServoPos7, rightServoPos1);
+              break;
+    case 'L': moveFlags(leftServoPos7, rightServoPos2);
+              break;
+    case 'R': moveFlags(leftServoPos7, rightServoPos3);
+              break;
+    case 'N': moveFlags(leftServoPos7, rightServoPos4);
+              break;
+    case 'E': moveFlags(leftServoPos7, rightServoPos5);
+              break;
+    case 'S': moveFlags(leftServoPos7, rightServoPos6);
+              break;
+    case 'W': moveFlags(leftServoPos7, rightServoPos7);
+              break;
+  }
+}
+
+// 3. Input: A string
+// Output: Split up the string into individual characters using the convertCharToFlagMovement function.
+// For each character, convert this into flag positions.
+void displayMessageFromString(String inputString) {
+  inputString.toLowerCase();
+  for (int i=0; i<inputString.length(); i++) {
+    Serial.print(inputString[i]);
+    Serial.print(" ");
+    convertCharToFlagMovement(inputString[i]);
+  }
+  Serial.println("*****************************************************");
+}
+
+// 4. Input: An array of strings, index, state of the ready to display message button
+// Output: if the ready to display message button was pressed, extract one string using the index.
+// Display the message of this string using the displayMessageFromString function.
+void chooseStringFromArrayToDisplayMessage(String arrayOfStrings[], int arrayIndex, int buttonStateChanged) {
+  if (buttonStateChanged == 1) {
+    displayMessageFromString(arrayOfStrings[arrayIndex]);
+    Serial.println("----------------------------------------------");
+  }
+} 
+
+void moveFlagsToRestingPosition(int buttonStateChanged) {
+  if (buttonStateChanged == 0) {
+    servoDelay = 0;
+    convertCharToFlagMovement(' ');
+  }
+}
+
+void playMessageBasedOnMode(int directionCode) {
+  if (directionCode != 0) {
+    servoDelay = 0;
+    // only look at the direction codes
+    // north position
+    if (directionCode == 1) {
+      convertCharToFlagMovement('U');
+    }
+    // south position
+    else if (directionCode == 2) {
+      convertCharToFlagMovement('D');
+    }
+    // west position
+    else if (directionCode == 3) {
+      convertCharToFlagMovement('L');
+    }
+    // east position
+    else if (directionCode == 4) {
+      convertCharToFlagMovement('R');
+    }
+    // north-east position
+    else if (directionCode == 6) {
+      convertCharToFlagMovement('E');
+    }
+    // south-east position
+    else if (directionCode == 7) {
+      convertCharToFlagMovement('S');
+    }
+    // north-west position
+    else if (directionCode == 8) {
+      convertCharToFlagMovement('N');
+    }
+    // south-west position
+    else if (directionCode == 9) {
+      convertCharToFlagMovement('W');
+    }
+    // rest/stop position
+    else if (directionCode == 5) {
+      convertCharToFlagMovement(' ');
+    }
+  } else {
+    servoDelay = servoMaxDelay;
+    // play message if ready
+    chooseStringFromArrayToDisplayMessage(navigatorMessagesArray, navigatorMessagesIndex, messageReadyToBePlayed);
+    // otherwise, move flags to resting position
+    moveFlagsToRestingPosition(messageReadyToBePlayed);
+  }
+}
